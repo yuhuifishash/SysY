0a97c979d84b8a58326eda35db35e0196c5d1edb
CH3COOL <kaku16@126.com>
2024-03-26 00:38:00 +0800

    fix:phidestruction_class_inherit

--- target/armv7/instruction_select/arm_phi_destruction.h
+++ target/armv7/instruction_select/arm_phi_destruction.h
@@ -1,7 +1,7 @@
 #ifndef ARM_PHI_DESTRUCTION_H
 #define ARM_PHI_DESTRUCTION_H
 #include "../../common/machine_passes/machine_phi_destruction.h"
-class ArmPhiDestruction : public MachinePass{
+class ArmPhiDestruction : public MachinePhiDestruction{
 protected:
     MachineBaseInstruction* GetMoveInstruction(int dest_reg,int src_reg);
     MachineBaseInstruction* GetMoveImmInstruction(int dest_reg,long long imm_value);
