0f8b20142f3225a22e39b3700175158d46f5e169
2110835-Hua Zhi Yuan <1479750073@qq.com>
2024-04-16 08:45:33 +0800

    update AliasAnalysis

--- optimize/alias_analysis/alias_analysis.cc
+++ optimize/alias_analysis/alias_analysis.cc
@@ -1,5 +1,6 @@
 #include "alias_analysis.h"
 
+extern std::map<std::string,CFG*> CFGMap;
 AliasAnalyser alias_analyser;
 
 AliasAnalyser::AliasResult AliasAnalyser::QueryAlias(Operand op1, Operand op2,CFG* C)
@@ -7,12 +8,30 @@ AliasAnalyser::AliasResult AliasAnalyser::QueryAlias(Operand op1, Operand op2,CF
     return AliasResult::MustAlias;
 }
 
-AliasAnalyser::ModRefResult AliasAnalyser::QueryInstModRef(Instruction* I,Operand op,CFG* C)
+AliasAnalyser::ModRefResult AliasAnalyser::QueryInstModRef(Instruction I,Operand op,CFG* C)
 {
     return ModRefResult::ModRef;
 }
 
+bool isFunctionArgs(CFG* C, Operand op)
+{
+    if(op->GetOperandType() == BasicOperand::REG){
+        int siz = C->function_def->formals.size();
+        int regno = ((RegOperand*)op)->GetRegNo();
+        return regno < siz; 
+    }
+    return false;
+}
+
 void AliasAnalyser::AliasAnalysis()
+{
+    for(auto [defI,cfg]:IR->llvm_cfg){
+        AliasAnalysis(cfg);
+    }
+
+}
+
+void AliasAnalyser::AliasAnalysis(CFG* C)
 {
 
 }
@@ -23,4 +42,10 @@ void AliasAnalysis(LLVMIR* IR)
 {
     alias_analyser.SetLLVMIR(IR);
     alias_analyser.AliasAnalysis();
+}
+
+
+void AliasAnalyser::PrintAAResult()
+{
+    
 }
--- optimize/alias_analysis/alias_analysis.h
+++ optimize/alias_analysis/alias_analysis.h
@@ -2,10 +2,27 @@
 #define ALIAS_ANALYSIS_H
 
 #include "../../include/ir.h"
+#include <assert.h>
+
+//in SysY, we only need to analyse Arrays, so the algorithm is very simple
+
+class MemRWInfo
+{
+public:
+    bool is_read_fullmem = false;
+    bool is_write_fullmem = false;
+    std::vector<Operand> ReadPtrs;
+    std::vector<Operand> WritePtrs;
+
+    bool isReadMem(){return ReadPtrs.size() != 0;}
+    bool isWriteMem(){return WritePtrs.size() != 0;}
+};
 
 class AliasAnalyser
 {
 private:
+    std::map<CFG*, MemRWInfo> CFGMemRWMap;
+    std::map<int, MemRWInfo> PtrRegMemMap;
     LLVMIR* IR;
 public:
     enum AliasResult{
@@ -20,8 +37,11 @@ public:
         Mod = 2,
         ModRef = 3,
     };
+
     void SetLLVMIR(LLVMIR* ir){this->IR = ir;}
+
     void AliasAnalysis();
+    void AliasAnalysis(CFG* C);//alias_analysis in single function
 
     //return alias information of op1 and op2
     //the type of op1 and op2 maybe RegOperand or GlobalOperand
@@ -31,7 +51,9 @@ public:
     //return modify/reference information of inst and op
     //return if the inst will read/write the op
     //(the op must be ptr)
-    ModRefResult QueryInstModRef(Instruction* I,Operand op, CFG* C);
+    ModRefResult QueryInstModRef(Instruction I,Operand op, CFG* C);
+
+    void PrintAAResult();
 };
 
 #endif
