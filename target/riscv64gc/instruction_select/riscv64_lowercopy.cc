af6c91c4932bd6b86d4f3cfdc2b8afbc331c3350
CH3COOL <kaku16@126.com>
2024-04-20 22:52:15 +0800

    RISCV_i32_lowercopy

--- target/riscv64gc/instruction_select/riscv64_lowercopy.cc
+++ target/riscv64gc/instruction_select/riscv64_lowercopy.cc
@@ -9,15 +9,26 @@ void RiscV64LowerCopy::Execute() {
                 if(ins->arch == MachineBaseInstruction::COPY){
                     auto m_copy = (MachineCopyInstruction*)ins;
                     assert(m_copy->dst->op_type == MachineBaseOperand::REG);
-                    auto RV_copy = new RiscV64Instruction();
                     if(m_copy->src->op_type == MachineBaseOperand::IMMI){
-                        // 
+                        int i32_imm = ((MachineImmediateInt*)(m_copy->src))->imm32;
+                        
+                        auto RV_lui = new RiscV64Instruction();
+                        auto RV_addi = new RiscV64Instruction();
+
+                        RV_lui->setOpcode(RISCV_LUI);
+                        RV_lui->setRd(((MachineRegister*)(m_copy->dst))->reg);
+                        RV_lui->setImm((i32_imm + 0x800) & 0xFFFFF000);// Reference: https://zhuanlan.zhihu.com/p/374235855
+
+                        RV_addi->setOpcode(RISCV_ADDI);
+                        RV_addi->setRd(((MachineRegister*)(m_copy->dst))->reg);
+                        RV_addi->setImm(i32_imm & 0xFFFFF000);
                     }else if(m_copy->src->op_type == MachineBaseOperand::IMMF){
                         assert(false);
                     }else if(m_copy->src->op_type == MachineBaseOperand::REG){
+                        auto RV_copy = new RiscV64Instruction();
                         RV_copy->setOpcode(RISCV_ADDI);
-                        // RV_copy->setRd(m_copy->dst);
-                        // RV_copy->setRs1(m_copy->src);
+                        RV_copy->setRd(((MachineRegister*)(m_copy->dst))->reg);
+                        RV_copy->setRs1(((MachineRegister*)(m_copy->src))->reg);
                         RV_copy->setImm(0);
                     }else{
                         assert(false);
