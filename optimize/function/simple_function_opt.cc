072f185fdc6242e60fe54114616a24b2e8d62e13
aakennes <rsq363570@outlook.com>
2024-04-23 16:14:38 +0800

    TRE update

--- optimize/function/simple_function_opt.cc
+++ optimize/function/simple_function_opt.cc
@@ -72,7 +72,12 @@ void MakeFunctionOneExit(CFG *C) {
  * this function will elimate tailrecursive
  * you can use testcase 087_gcd.sy to check
  * you should set CFG::max_reg correctly
- * @param C the control flow graph of the function */
+ * @param C the control flow graph of the function 
+ * First,you should check if ptr's params would be changed in call.
+ * If yes,create AllocaInstruction and StoreInstruction for the changed ptrs
+ * Next,add load ptr before using the changed ptrs
+ * Finally,deal with non-ptr params
+ * */
 void TailRecursiveEliminate(CFG *C) {
     auto FuncdefI = C->function_def;
     if (FuncdefI->GetFormalSize() > 5) {
@@ -85,12 +90,20 @@ void TailRecursiveEliminate(CFG *C) {
     std::vector<Operand> PtrArr;    // store newptr equal to oldparam
     std::set<Instruction> EraseSet;
     // when exist call ptr, ret
+
+    //@NeedtoInsertPTR:check if ptr in function params need to be insert 
     for (auto [id, bb] : *C->block_map) {
+        if(NeedtoInsertPTR){
+            break;
+        }
         if (bb->Instruction_list.back()->GetOpcode() != RET) {
             continue;
         }
         auto retI = (RetInstruction *)bb->Instruction_list.back();
         for (auto I : bb->Instruction_list) {
+            if(NeedtoInsertPTR){
+                break;
+            }
             if (I->GetOpcode() != CALL) {
                 continue;
             }
@@ -102,40 +115,15 @@ void TailRecursiveEliminate(CFG *C) {
             callI->GetResult() != NULL && callI->GetResult()->GetFullName() == retI->GetRetVal()->GetFullName();
             bool opt2 = callI->GetResult() == NULL && retI->GetType() == VOID;
             if (opt1 || opt2) {
-                EraseSet.insert(callI);
-                EraseSet.insert(retI);
                 auto list_size = callI->GetParameterList().size();
-                auto bb0_it = --bb0->Instruction_list.end();
-
-                auto bb0_ptr_it = bb0->Instruction_list.begin();
-                while ((*bb0_ptr_it)->GetOpcode() == ALLOCA) {
-                    bb0_ptr_it++;
-                }
-                // if exist alloca ptr,bb0_ptr_it=the end of alloca ptr
                 for (auto i = 0; i < list_size; i++) {
                     auto callI_reg = (RegOperand *)(callI->GetParameterList()[i].second);
                     if (callI_reg->GetRegNo() == i) {
                         continue;
                     }    // funtion params id stand by i
                     NeedtoInsertPTR = 1;
-                    Instruction allocaI;
-                    if (callI->GetParameterList()[i].first == PTR) {
-                        bb0_ptr_it--;
-                        allocaI = *bb0_ptr_it;
-                    } else {
-                        bb0_it--;
-                        allocaI = *bb0_it;
-                        while (allocaI->GetOpcode() != ALLOCA) {
-                            bb0_it--;
-                            allocaI = *bb0_it;
-                        }
-                    }
-
-                    auto storeI = new StoreInstruction(callI->GetParameterList()[i].first, allocaI->GetResultReg(),
-                                                       callI->GetParameterList()[i].second);
-                    bb->InsertInstruction(1, storeI);
+                    break;
                 }
-                bb->InsertInstruction(1, new BrUncondInstruction(new LabelOperand(1)));
             }
         }
     }
@@ -151,22 +139,14 @@ void TailRecursiveEliminate(CFG *C) {
         }
     }
 
-    while (!StoreDeque.empty()) {
-        bb0->InsertInstruction(0, StoreDeque.back());
-        StoreDeque.pop_back();
-    }
-    for (auto *it : AllocaDeque) {
-        bb0->InsertInstruction(0, it);
-    }
+    //insert load before new ptr
     for (auto [id, bb] : *C->block_map) {
         auto tmp_Instruction_list = bb->Instruction_list;
         bb->Instruction_list.clear();
         for (auto I : tmp_Instruction_list) {
-            if (EraseSet.find(I) != EraseSet.end()) {
-                continue;
-            }
             auto ResultOperands = I->GetNonResultOperands();
             bool NeedtoUpdate = 0;
+            //jump call self() ret
             if (id != 0 && NeedtoInsertPTR && !ResultOperands.empty()) {
                 for (u_int32_t i = 0; i < ResultOperands.size(); ++i) {
                     auto ResultReg = ResultOperands[i];
@@ -188,8 +168,79 @@ void TailRecursiveEliminate(CFG *C) {
             bb->InsertInstruction(1, I);
         }
     }
+    
+
+    while (!StoreDeque.empty()) {
+        bb0->InsertInstruction(0, StoreDeque.back());
+        StoreDeque.pop_back();
+    }
+    for (auto *it : AllocaDeque) {
+        bb0->InsertInstruction(0, it);
+    }
     while (!AllocaDeque.empty()) {
         AllocaDeque.pop_back();
+    }
+    for (auto [id, bb] : *C->block_map) {
+        if (bb->Instruction_list.back()->GetOpcode() != RET) {
+            continue;
+        }
+        auto retI = (RetInstruction *)bb->Instruction_list.back();
+        for (auto I : bb->Instruction_list) {
+            if (I->GetOpcode() != CALL) {
+                continue;
+            }
+            auto callI = (CallInstruction *)I;
+            if (callI->GetFunctionName() != FuncdefI->GetFunctionName()) {
+                continue;
+            }
+            bool opt1 =
+            callI->GetResult() != NULL && callI->GetResult()->GetFullName() == retI->GetRetVal()->GetFullName();
+            bool opt2 = callI->GetResult() == NULL && retI->GetType() == VOID;
+            if (opt1 || opt2) {
+                EraseSet.insert(callI);
+                EraseSet.insert(retI);
+                auto list_size = callI->GetParameterList().size();
+                auto bb0_it = --bb0->Instruction_list.end();
+                auto bb0_ptr_it = bb0->Instruction_list.begin();
+                while ((*bb0_ptr_it)->GetOpcode() == ALLOCA) {
+                    bb0_ptr_it++;
+                }
+                // if exist alloca ptr,bb0_ptr_it=the end of alloca ptr
+                for (auto i = 0; i < list_size; i++) {
+                    auto callI_reg = (RegOperand *)(callI->GetParameterList()[i].second);
+                    if (callI_reg->GetRegNo() == i) {
+                        continue;
+                    }    // funtion params id stand by i
+                    Instruction allocaI;
+                    if (callI->GetParameterList()[i].first == PTR) {
+                        bb0_ptr_it--;
+                        allocaI = *bb0_ptr_it;
+                    } else {
+                        bb0_it--;
+                        allocaI = *bb0_it;
+                        while (allocaI->GetOpcode() != ALLOCA) {
+                            bb0_it--;
+                            allocaI = *bb0_it;
+                        }
+                    }
+
+                    auto storeI = new StoreInstruction(callI->GetParameterList()[i].first, allocaI->GetResultReg(),
+                                                       callI->GetParameterList()[i].second);
+                    bb->InsertInstruction(1, storeI);
+                }
+                bb->InsertInstruction(1, new BrUncondInstruction(new LabelOperand(1)));
+            }
+        }
+    }
+    for (auto [id, bb] : *C->block_map) {
+        auto tmp_Instruction_list = bb->Instruction_list;
+        bb->Instruction_list.clear();
+        for (auto I : tmp_Instruction_list) {
+            if (EraseSet.find(I) != EraseSet.end()) {
+                continue;
+            }
+            bb->InsertInstruction(1, I);
+        }
     }
     EraseSet.clear();
     C->BuildCFG();
