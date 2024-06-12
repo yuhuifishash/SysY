bc6498c8fb72d79c3c962e89cebb5580546f2de5
2110835-Hua Zhi Yuan <1479750073@qq.com>
2024-06-12 12:24:16 +0800

    update loopdep analysis and parallel

--- optimize/loop/loop_parallel.cc
+++ optimize/loop/loop_parallel.cc
@@ -1,6 +1,9 @@
 #include "../../include/cfg.h"
 #include "../../include/ir.h"
+#include "../alias_analysis/alias_analysis.h"
 #include <functional>
+
+extern AliasAnalyser *alias_analyser;
 
 void AddParallelLib(LLVMIR* IR) {
     auto ThreadCreateI_111 = new FunctionDeclareInstruction(VOID,"___parallel_loop_constant_111",true);
@@ -32,10 +35,56 @@ void AddParallelLib(LLVMIR* IR) {
     IR->function_declare.push_back(ThreadCreateI_000);
 }
 
+NaturalLoop::LoopDepResult NaturalLoop::CheckDependenceResult(Instruction I1, Instruction I2){ 
+    return ANY;
+}
 
-bool NaturalLoop::LoopDataDependenceTest(CFG *C) {
-    
-    return false; 
+
+//if the loop can be parallel, return true
+bool NaturalLoop::LoopCarriedDependenceTest(CFG *C) {
+    if(scev.is_simpleloop == false){
+        return false;
+    }
+    std::map<int, Instruction> ResultMap;
+    std::vector<LoadInstruction*> LoadList;
+    std::vector<StoreInstruction*> StoreList;
+
+    for (auto [id, bb] : *C->block_map) {
+        for (auto I : bb->Instruction_list) {
+            int v = I->GetResultRegNo();
+            if (v != -1) {    // result exists
+                ResultMap[v] = I;
+            }
+        }
+    }
+
+    for(auto n : loop_nodes){
+        for(auto I : n->Instruction_list){
+            if(I->GetOpcode() == LOAD){
+                LoadList.push_back((LoadInstruction*)I);
+            }else if(I->GetOpcode() == STORE){
+                StoreList.push_back((StoreInstruction*)I);
+            }
+        }
+    }
+
+    for(auto LoadI:LoadList){
+        for(auto StoreI:StoreList){
+            auto ptr1 = LoadI->GetPointer();
+            auto ptr2 = StoreI->GetPointer();
+            if(alias_analyser->QueryAlias(ptr1,ptr2,C) != AliasAnalyser::NoAlias){
+                //alias, may generate dependency
+                auto GEPI1 = ResultMap[((RegOperand*)ptr1)->GetRegNo()];
+                auto GEPI2 = ResultMap[((RegOperand*)ptr2)->GetRegNo()];
+                auto result = CheckDependenceResult(GEPI1,GEPI2);
+                if(result != NONE){
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true; 
 }
 
 void LoopParallel(CFG *C) {
@@ -57,7 +106,7 @@ void LoopParallel(CFG *C) {
 }
 
 bool NaturalLoop::LoopParallel(CFG *C) {
-    if (!LoopDataDependenceTest(C)) {
+    if (!LoopCarriedDependenceTest(C)) {
         return false;
     }
 
