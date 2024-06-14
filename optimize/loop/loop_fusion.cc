5ed1271d24941c5dfc980e55ff3f3944d06ccf64
aakennes <rsq363570@outlook.com>
2024-06-14 13:26:59 +0800

    update

--- optimize/loop/loop_fusion.cc
+++ optimize/loop/loop_fusion.cc
@@ -1,3 +1,24 @@
 #include "../../include/cfg.h"
-#include "../../include/ir.h"
 
+static bool LoopContinuouslyCheck(NaturalLoop* L1, NaturalLoop* L2) {
+
+}
+
+//I1 and I2 must be GEP
+static bool LoopAntiDepSingleInstCheck(NaturalLoop* L1, NaturalLoop* L2, Instruction* I1, Instruction* I2) {
+
+}
+
+static bool LoopAntiDependencyCheck(NaturalLoop* L1, NaturalLoop* L2) {
+
+}
+
+void LoopFusion(CFG *C) {
+    for(auto l1:C->LoopForest.loop_set){
+        for(auto l2:C->LoopForest.loop_set){
+            if(l1 == l2){
+                continue;
+            }
+        }
+    }
+}
