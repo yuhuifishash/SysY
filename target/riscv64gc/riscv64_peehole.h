422e1162a84e59b823d8a9718a2e254295976bcc
CH3COOL <kaku16@126.com>
2024-06-29 20:44:49 +0800

    gep

---
+++ target/riscv64gc/riscv64_peehole.h
@@ -0,0 +1,17 @@
+#ifndef RISCV64_PEEHOLE_H
+#define RISCV64_PEEHOLE_H
+#include "riscv64.h"
+
+class RiscV64SSAPeehole : public MachinePass {
+    const int length = 10;
+public:
+    RiscV64SSAPeehole(MachineUnit* unit) : MachinePass(unit) {}
+    void Execute();
+};
+
+class RiscV64SSADeadDefElimate : public MachinePass {
+public:
+    RiscV64SSADeadDefElimate(MachineUnit* unit) : MachinePass(unit) {}
+    void Execute();
+};
+#endif
