3fc02e39a20671734429522675a410bef101bcc0
2110835-Hua Zhi Yuan <1479750073@qq.com>
2024-07-11 21:14:09 +0800

    update cse

--- target/riscv64gc/optimize/riscv64_branch_predict.h
+++ target/riscv64gc/optimize/riscv64_branch_predict.h
@@ -2,9 +2,10 @@
 #define RISCV64_BRANCH_PREDICT
 #include "../riscv64.h"
 class RiscV64BranchPredict : public MachinePass{
+private:
+    void StaticBranchPredict();
 public:
     RiscV64BranchPredict(MachineUnit* unit) : MachinePass(unit){}
     void Execute();
-    void StaticBranchPredict();
 };
 #endif
--- target/riscv64gc/optimize/riscv64_cse.h
+++ target/riscv64gc/optimize/riscv64_cse.h
@@ -2,6 +2,8 @@
 #define RISCV64_CSE
 #include "../riscv64.h"
 class RiscV64CSE : public MachinePass{
+private:
+    void ReplaceRegByMapInCurrFunc(std::map<int,int> regreplace_map);
 public:
     RiscV64CSE(MachineUnit* unit) : MachinePass(unit){}
     void Execute();
