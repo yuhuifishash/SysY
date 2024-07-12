592a96188026bf7c8b473867ce49206eb2d91e78
2110835-Hua Zhi Yuan <1479750073@qq.com>
2024-07-12 20:37:41 +0800

    add loop_depth

--- target/common/machine_instruction_structures/machine.h
+++ target/common/machine_instruction_structures/machine.h
@@ -22,7 +22,9 @@ private:
     MachineFunction *parent;
 
 public:
+    int loop_depth = 0;
     std::vector<float> branch_predictor; 
+
     virtual std::list<MachineBaseInstruction *>::iterator getInsertBeforeBrIt() = 0;
     void InsertParallelCopyList(Register dst, MachineBaseOperand *src) { parallel_copy_list[dst] = src; }
     decltype(parallel_copy_list) &GetParallelCopyList() { return parallel_copy_list; }
