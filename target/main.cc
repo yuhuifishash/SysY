5d6f35200ce08877bd897abeb482131b31ff40a9
2110835-Hua Zhi Yuan <1479750073@qq.com>
2024-08-14 21:40:23 +0800

    fix main

--- target/main.cc
+++ target/main.cc
@@ -273,7 +273,7 @@ int main(int argc, char **argv) {
         llvmIR.PassExecutor(SimpleDSE);
         llvmIR.PassExecutor(SimpleDCE);
         
-        llvmIR.PassExecutor(GlobalCodeMotion);
+        // llvmIR.PassExecutor(GlobalCodeMotion);
         // TODO():GVN/GCM
         
         for (int i = 0; i < 5; ++i) {
